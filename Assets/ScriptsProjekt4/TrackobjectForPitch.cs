eab0d0f4904f81dc66da2af0a4539a6e1b0fda5c
Chris <christian.friedrich.bauer04@gmail.com>
2026-02-10 14:19:58 +0100

    not hearing shite

--- Assets/ScriptsProjekt4/TrackobjectForPitch.cs
+++ Assets/ScriptsProjekt4/TrackobjectForPitch.cs
@@ -1,10 +1,14 @@
+using Oculus.Interaction;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit.Interactors;
+
 //maybe try including the ResetObjectPosition here instead of another script
 public class TrackobjectForPitch : MonoBehaviour
 {
-    AudioSource audiosource;
+    AudioSource  audiosource;
     [SerializeField] int startpitch;
     [SerializeField] AudioClip audioResource;
+    Vector3 originalpos;
     private float pitchValue;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -12,18 +16,18 @@ public class TrackobjectForPitch : MonoBehaviour
     {
         audiosource = GetComponent<AudioSource>();
         audiosource.pitch = startpitch;
+        originalpos = gameObject.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(0))
-        {
             audiosource.PlayOneShot(audioResource);
 
-            pitchValue = gameObject.transform.GetChild(0).position.y;
+            pitchValue = gameObject.transform.position.y;
             print(pitchValue);
             audiosource.pitch = startpitch + pitchValue;
-        }
+        
+
     }
 }
