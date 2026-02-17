5c57c162e1eb8fd42a7feee590b5c534533c1cde
Chris <christian.friedrich.bauer04@gmail.com>
2026-02-17 23:24:47 +0100

    ai code for fountains

--- Assets/ScriptsProjekt4/moveOnHover.cs
+++ Assets/ScriptsProjekt4/moveOnHover.cs
@@ -1,18 +1,22 @@
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 using UnityEngine.XR.Interaction.Toolkit.Interactables;
+using System.Collections;
 
+[RequireComponent(typeof(XRGrabInteractable))]
 public class MoveOnHover : MonoBehaviour
 {
     private XRGrabInteractable grabInteractable;
-    private Vector3 originalPos;
+
+    private Vector3 hoverStartPos;
+    private Coroutine moveRoutine;
 
     public Vector3 hoverOffset = new Vector3(0, 0.1f, 0);
+    public float moveDuration = 0.15f;
 
-    void Start()
+    void Awake()
     {
         grabInteractable = GetComponent<XRGrabInteractable>();
-        originalPos = transform.position;
 
         grabInteractable.hoverEntered.AddListener(OnHoverEnter);
         grabInteractable.hoverExited.AddListener(OnHoverExit);
@@ -20,11 +24,41 @@ public class MoveOnHover : MonoBehaviour
 
     void OnHoverEnter(HoverEnterEventArgs args)
     {
-        transform.position = originalPos + hoverOffset;
+        hoverStartPos = transform.position;
+        StartSmoothMove(hoverStartPos + hoverOffset);
     }
 
     void OnHoverExit(HoverExitEventArgs args)
     {
-        transform.position = originalPos;
+        StartSmoothMove(hoverStartPos);
+    }
+
+    void StartSmoothMove(Vector3 target)
+    {
+        if (moveRoutine != null)
+            StopCoroutine(moveRoutine);
+
+        moveRoutine = StartCoroutine(SmoothMove(target));
+    }
+
+    IEnumerator SmoothMove(Vector3 target)
+    {
+        Vector3 start = transform.position;
+        float time = 0f;
+
+        while (time < moveDuration)
+        {
+            transform.position = Vector3.Lerp(start, target, time / moveDuration);
+            time += Time.deltaTime;
+            yield return null;
+        }
+
+        transform.position = target;
+    }
+
+    void OnDestroy()
+    {
+        grabInteractable.hoverEntered.RemoveListener(OnHoverEnter);
+        grabInteractable.hoverExited.RemoveListener(OnHoverExit);
     }
 }
