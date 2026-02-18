b005935bd6e37b1d3f02cf606dcb994266de1220
Chris <christian.friedrich.bauer04@gmail.com>
2026-02-18 16:20:08 +0100

    fountain bullshit version

--- Assets/ScriptsProjekt4/moveOnHover.cs
+++ Assets/ScriptsProjekt4/moveOnHover.cs
@@ -1,64 +1,41 @@
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 using UnityEngine.XR.Interaction.Toolkit.Interactables;
-using System.Collections;
 
 [RequireComponent(typeof(XRGrabInteractable))]
-public class MoveOnHover : MonoBehaviour
+public class moveOnHover : MonoBehaviour
 {
     private XRGrabInteractable grabInteractable;
 
-    private Vector3 hoverStartPos;
-    private Coroutine moveRoutine;
+    private Vector3 originalPos;
 
-    public Vector3 hoverOffset = new Vector3(0, 0.1f, 0);
-    public float moveDuration = 0.15f;
 
-    void Awake()
+    private bool isGrabbed = false;
+
+    void Start()
     {
         grabInteractable = GetComponent<XRGrabInteractable>();
 
-        grabInteractable.hoverEntered.AddListener(OnHoverEnter);
-        grabInteractable.hoverExited.AddListener(OnHoverExit);
+        originalPos = transform.position;
+
+        grabInteractable.selectEntered.AddListener(OnGrab);
+        grabInteractable.selectExited.AddListener(OnRelease);
     }
 
-    void OnHoverEnter(HoverEnterEventArgs args)
+    void OnGrab(SelectEnterEventArgs args)
     {
-        hoverStartPos = transform.position;
-        StartSmoothMove(hoverStartPos + hoverOffset);
+        isGrabbed = true;
     }
 
-    void OnHoverExit(HoverExitEventArgs args)
+    void OnRelease(SelectExitEventArgs args)
     {
-        StartSmoothMove(hoverStartPos);
+        isGrabbed = false;
+
+        transform.position = originalPos;
     }
 
-    void StartSmoothMove(Vector3 target)
+    void Update()
     {
-        if (moveRoutine != null)
-            StopCoroutine(moveRoutine);
 
-        moveRoutine = StartCoroutine(SmoothMove(target));
-    }
-
-    IEnumerator SmoothMove(Vector3 target)
-    {
-        Vector3 start = transform.position;
-        float time = 0f;
-
-        while (time < moveDuration)
-        {
-            transform.position = Vector3.Lerp(start, target, time / moveDuration);
-            time += Time.deltaTime;
-            yield return null;
-        }
-
-        transform.position = target;
-    }
-
-    void OnDestroy()
-    {
-        grabInteractable.hoverEntered.RemoveListener(OnHoverEnter);
-        grabInteractable.hoverExited.RemoveListener(OnHoverExit);
     }
 }
